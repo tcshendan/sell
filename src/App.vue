bfbc37164315fa7615c8df16ca92b6287d06a9ee
tcshendan <664542051@qq.com>
2018-03-24 16:20:03 +0800

    add vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-header></v-header>
-    <div class="tab">
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
       <div class="tab-item">
         <a v-link="{path:'/goods'}">商品</a>
       </div>
@@ -16,10 +16,26 @@
   </div>
 </template>
 
-<script>
+<script type="ecmascript-6">
   import header from 'components/header/header.vue';
 
+  const ERR_OK = 0;
+
   export default {
+    data () {
+      return {
+        seller: {}
+      };
+    },
+    created () {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if(response.errno === ERR_OK) {
+          this.seller = response.data;
+          console.log(this.seller);
+        }
+      });
+    },
     components: {
       'v-header': header
     }
@@ -28,11 +44,14 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
   .tab
     display: flex
     width: 100%
     height: 40px
     line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
       flex: 1
       text-align: center
