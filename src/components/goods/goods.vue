919f7d5954583fe3a87b3ccfcfad6d535f1545b3
shendan <664542051@qq.com>
2018-04-09 19:09:48 +0800

    完成cartcontrol组件静态样式

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -28,6 +28,9 @@
                 <div class="price">
                   <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
               </div>
             </li>
           </ul>
@@ -41,6 +44,7 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import shopcart from 'components/shopcart/shopcart';
+  import cartcontrol from 'components/cartcontrol/cartcontrol';
 
   const ERR_OK = 0;
 
@@ -115,7 +119,8 @@
       }
     },
     components: {
-      shopcart
+      shopcart,
+      cartcontrol
     }
   };
 </script>
@@ -221,4 +226,8 @@
             .old
               text-decoration: line-through
               font-size: 10px
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 12px        
 </style>
