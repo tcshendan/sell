52a98980c30b4d61dc5e74eb4385ed07dcb4f705
shendan <664542051@qq.com>
2018-04-18 19:42:26 +0800

    增加food组件content样式和BScroll

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-show="showFlag" class="food" transition="move">
+  <div v-show="showFlag" class="food" transition="move" v-el:food>
     <div class="food-content">
       <div class="image-header">
         <img :src="food.image">
@@ -18,11 +18,17 @@
           <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
         </div>
       </div>
+      <div class="cartcontrol-wrapper">
+        <cartcontrol :food="food"></cartcontrol>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+  import cartcontrol from 'components/cartcontrol/cartcontrol';
+
   export default {
     props: {
       food: {
@@ -37,10 +43,22 @@
     methods: {
       show () {
         this.showFlag = true;
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$els.food, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
       },
       hide () {
         this.showFlag = false;
       }
+    },
+    components: {
+      cartcontrol
     }
   };
 </script>
@@ -79,4 +97,32 @@
           padding: 10px
           font-size: 20px
           color: #fff
+    .content
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height: 10px
+        height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          font-size: 10px
 </style>
