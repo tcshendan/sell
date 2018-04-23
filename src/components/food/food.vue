0c05ca2c5f1df3fed7b22763128e082853d19bd4
shendan <664542051@qq.com>
2018-04-23 18:16:19 +0800

    完成商品详情页ratingselect组件dom结构“

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -27,6 +27,11 @@
         <h1 class="title">商品信息</h1>
         <p class="text">{{food.info}}</p>
       </div>
+      <split></split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +41,11 @@
   import Vue from 'vue';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
   import split from 'components/split/split';
+  import ratingselect from 'components/ratingselect/ratingselect';
+
+  // const POSITIVE = 0;
+  // const NEGATIVE = 1;
+  const ALL = 2;
 
   export default {
     props: {
@@ -45,12 +55,21 @@
     },
     data () {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show () {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = true;
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$els.food, {
@@ -74,7 +93,8 @@
     },
     components: {
       cartcontrol,
-      split
+      split,
+      ratingselect
     }
   };
 </script>
@@ -176,4 +196,11 @@
         padding: 0 8px
         font-size: 12px
         color: rgb(77, 85, 93)
-</style>
+    .rating
+      padding-top: 18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+  </style>
