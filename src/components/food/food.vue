1848951cfac57c0aa3ecf9640585a1ec1830e3d1
shendan <664542051@qq.com>
2018-04-26 17:42:16 +0800

    完成商品详情页评价列表时间转换

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -38,7 +38,7 @@
                 <span class="name">{{rating.username}}</span>
                 <img class="avatar" width="12" height="12" :src="rating.avatar">
               </div>
-              <div class="time">{{rating.rateTime}}</div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
               <p class="text">
                 <span :class="{'icon-thumb_up':rating.rateType===0, 'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
               </p>
@@ -54,6 +54,7 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import Vue from 'vue';
+  import {formatDate} from 'common/js/date';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
   import split from 'components/split/split';
   import ratingselect from 'components/ratingselect/ratingselect';
@@ -126,6 +127,12 @@
         this.$nextTick(() => {
           this.scroll.refresh();
         });
+      }
+    },
+    filters: {
+      formatDate (time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
       }
     },
     components: {
