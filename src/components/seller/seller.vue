2f12bc5055fcfc0487a66b0e3e3cedbf57d8673b
tcshendan <664542051@qq.com>
2018-04-30 23:56:33 +0800

    seller组件收藏商家开发

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,6 +28,10 @@
             </div>
           </li>
         </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active': favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
       </div>
       <split></split>
       <div class="bulletin">
@@ -75,6 +79,16 @@
         type: Object
       }
     },
+    data () {
+      return {
+        favorite: false
+      };
+    },
+    computed: {
+      favoriteText () {
+        return this.favorite ? '已收藏' : '收藏';
+      }
+    },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     },
@@ -89,6 +103,12 @@
       this._initPics();
     },
     methods: {
+      toggleFavorite (event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
+      },
       _initScroll () {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$els.seller, {
@@ -136,6 +156,7 @@
     overflow: hidden
     .overview
       padding: 18px
+      position: relative
       .title
         margin-bottom: 8px
         line-height: 14px
@@ -177,6 +198,24 @@
             color: rgb(7, 17, 27)
             .stress
               font-size: 24px
+      .favorite
+        position: absolute
+        width: 50px
+        right: 11px
+        top: 18px
+        text-align: center
+        .icon-favorite
+          display: block
+          margin-bottom: 4px
+          line-height: 24px
+          font-size: 24px
+          color: #d4d6d9
+          &.active
+            color: rgb(240, 20, 20)
+        .text
+          line-height: 10px
+          font-size: 14px
+          color: rgb(77, 85, 93)
     .bulletin
       padding: 18px 18px 0 18px
       .title
